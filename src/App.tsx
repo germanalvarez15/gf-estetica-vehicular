d08d7d6c33e5e2c82dedcc67f72108ce9c3362b7
German <german.alvarez@ideati.net>
2023-05-06 13:40:42 -0300

    first settings ready

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar/Navbar';
+import React from 'react';
+import './styles/components/app.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
